7b64a28ab922daffebe724cb972e2491c5a9738a
Deekshi-vuppala-222 <vuppaladeeksh@gmail.com>
2025-02-03 20:19:20 +0530

    react

---
+++ 598/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import React from "react";
+// function App() {
+//   let username="deekshitha";
+//   // let ele=React.createElement("div",{className:"App"},
+//   //   React.createElement("h1","null","header")
+//   // )
+//   return (
+    
+//     // <div className="App">
+//     //   <h1>Deekshitha</h1>
+//     // </div>
+//     // ele
+//     <div>
+//       <h1></h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+class App extends React.Component{
+  render(){
+    return(
+      <h1>Class Based component</h1>
+    )
+  }
+}
+export default App;
